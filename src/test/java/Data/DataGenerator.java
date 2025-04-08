f860ea465bb8d21f1e8fadc9d6b5b877e606c60a
Andrey <bawoper@gmail.com>
2025-04-08 23:00:24 +0300

    first commit

---
+++ src/test/java/Data/DataGenerator.java
@@ -0,0 +1,48 @@
+package Data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        String[] cities = new String[]{"Москва", "Смоленск", "Казань", "Барнаул", "Тверь", "Брянск", "Новосибирск",
+                "Красноярск", "Воронеж"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locate) {
+        Faker faker = new Faker(new Locale(locate));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+    public static class Registration {
+        private Registration() {
+        }
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo (generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+
+    }
+}
